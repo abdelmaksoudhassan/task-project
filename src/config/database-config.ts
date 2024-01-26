84325b5bf3ee80c4bc748c6516b3707c6753480a
Abdelmaksoud Hassan <abdelmaksoudhassan67@gmail.com>
2024-01-26 20:45:34 +0200

    make it better

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -2,20 +2,21 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { join } from "path";
 
 export const typeOrmConfig:TypeOrmModuleOptions = {
-    // type:'postgres',
-    // host:'localhost',
-    // port:5432,
-    // username:'postgres',
-    // password:'root',
-    // database:'grahQL',
-    // entities:[join(__dirname,'./../**','*.entity.{js,ts}')],
-    // synchronize:true
-    type:'postgres',
-    host:'bxutq9ydddgpfwmcy9pc-postgresql.services.clever-cloud.com',
-    port:50013,
-    username:'unwdtmydyqquddhkd5nr',
-    password:'XgfKQJK201ElNQcc465SZdQ2SYIEEK',
-    database:'bxutq9ydddgpfwmcy9pc',
-    entities:[join(__dirname,'./../**','*.entity.{js,ts}')],
-    synchronize:true
+    // type: 'mysql',
+    // host: 'localhost',
+    // port: 3306,
+    // username: 'root',
+    // password: 'root',
+    // database: 'taskdb',
+    // entities: [join(__dirname,'./../**','*.entity.{js,ts}')],
+    // synchronize: true
+
+    type: 'mysql',
+    host: 'bkb5gwwcrgqbt9aaqxs3-mysql.services.clever-cloud.com',
+    port: 3306,
+    username: 'uilq33jwgmxlxsgl',
+    password: '5GmjcutijcVbLeVPcYo5',
+    database: 'bkb5gwwcrgqbt9aaqxs3',
+    entities: [join(__dirname,'./../**','*.entity.{js,ts}')],
+    synchronize: true
 }
